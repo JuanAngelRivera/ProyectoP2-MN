bf7dec9bd10dc77f9012517c8cc12f8ffe477b59
Cherrisita <juangelrt21@gmail.com>
2025-02-27 11:16:54 -0600

    Primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.proyecto2pmn {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.proyecto2pmn to javafx.fxml;
+    exports com.example.proyecto2pmn;
+}
