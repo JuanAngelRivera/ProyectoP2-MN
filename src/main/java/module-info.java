d27fb4348dcc7128bb3291d4dd9c77df1ed767cf
Cherrisita <juangelrt21@gmail.com>
2025-02-27 12:08:57 -0600

    Funciona en una maquina

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.proyecto2pmn
 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.kordamp.bootstrap.core;
 
     opens com.example.proyecto2pmn to javafx.fxml;
     exports com.example.proyecto2pmn;
