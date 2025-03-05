f8c1412c242add284c36899908cb57bce343916a
Cherrisita <juangelrt21@gmail.com>
2025-03-05 11:13:33 -0600

    Ecuacion.java

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Cosas a realizar:
 
-- [ ] Crear una clase padre Ecuación
+- [x] Crear una clase padre Ecuación
 - [x] Encontrar una forma de reconocer las ecuaciones
-- [ ] Encontrar una forma de graficar las ecuaciones
+- [x] Encontrar una forma de graficar las ecuaciones
 - [ ] Heredar de Ecuación para cada método (regla falsa, newton-rhapson, gauss-jordan)
 - [ ] Generar interfaces
 - [ ] Crear tablas (ObservableList)
--- src/main/java/com/example/proyecto2pmn/Ecuacion.java
+++ src/main/java/com/example/proyecto2pmn/Ecuacion.java
@@ -1,6 +1,8 @@
 package com.example.proyecto2pmn;
 
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import org.nfunk.jep.JEP;
 import org.nfunk.jep.Node;
 import org.nfunk.jep.ParseException;
@@ -10,7 +12,6 @@ public class Ecuacion
     public String funcion;
     private JEP jep;
     private Node funcionTraducida;
-    private LineChart <Number, Number> gráfica;
 
     private JEP configJep()
     {
@@ -35,11 +36,47 @@ public class Ecuacion
         }
     }
 
+    private double evaluarFuncion(double x)
+    {
+        try
+        {
+            jep.setVarValue("x", x);
+            return (double)(jep.evaluate(funcionTraducida));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public LineChart <Number, Number> graficarFuncion(double minX, double maxX, double paso)
+    {
+        NumberAxis rangoX = new NumberAxis("x", minX, maxX, (maxX - minX) / 10);
+        NumberAxis rangoY = new NumberAxis();
+        rangoY.setLabel("f(x)");
+
+        LineChart < Number, Number > lineChart = new LineChart<>(rangoX, rangoY);
+        lineChart.setTitle("Gráfica de " + funcion);
+
+        XYChart.Series < Number, Number > series = new XYChart.Series<>();
+        series.setName("Funcion");
+
+        for (double x = minX; x <= maxX; x += paso)
+        {
+            double y = evaluarFuncion(x);
+            if (!Double.isNaN(y) && !Double.isInfinite(y))
+                series.getData().add(new XYChart.Data(x, y));
+        }
+
+        lineChart.getData().add(series);
+        return lineChart;
+    }
+
     Ecuacion(String funcion)
     {
         this.funcion = funcion;
         this.jep = configJep();
         traducirFuncion();
     }
-
 }
