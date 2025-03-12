69359b9ba4268062c7d666a76ab93f59023802d2
LunaDiegoLeo <diegolhl678@gmail.com>
2025-03-12 17:01:05 -0600

    Integracion del nuevo metodo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.proyecto2pmn
     requires jep;
     requires djep;
 
+
     opens com.example.proyecto2pmn to javafx.fxml;
     exports com.example.proyecto2pmn;
     exports com.example.proyecto2pmn.newtonRhapson;
