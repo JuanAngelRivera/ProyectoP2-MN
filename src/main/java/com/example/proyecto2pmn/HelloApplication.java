aaa280505583e9a325e9ca3e5edba7ade2d00f84
Cherrisita <juangelrt21@gmail.com>
2025-04-03 12:45:59 -0600

    Info ContextMenu

--- src/main/java/com/example/proyecto2pmn/HelloApplication.java
+++ src/main/java/com/example/proyecto2pmn/HelloApplication.java
@@ -2,26 +2,32 @@ package com.example.proyecto2pmn;
 
 import com.example.proyecto2pmn.NRmultivariable.Algoritmo;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Side;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application
 {
     private MenuBar menu;
-    private Menu ecuaciones, abiertos, cerrados, lineales, extra, regresion;
+    private Menu ecuaciones, abiertos, cerrados, lineales, extra, regresion, botonSalir;
     private MenuItem reglaFalsa, newtonRhapson, gaussJordan, gaussSeidel, gaussJordanMV, info, regresionLineal,
             regresionLinealMultiple;
     private VBox vbox;
     private Scene escena;
+    private ContextMenu infoContextMenu;
 
     public void create_ui()
     {
+        botonSalir = new Menu("Salir");
+        botonSalir.getItems().add(new MenuItem());
+        botonSalir.setOnShowing(event -> Platform.exit());
+
+
         reglaFalsa = new MenuItem("Regla Falsa");
         reglaFalsa.setOnAction(e -> new Interfaz(new com.example.proyecto2pmn.reglaFalsa.Algoritmo()));
 
@@ -43,7 +49,7 @@ public class HelloApplication extends Application
         //regresionLinealMultiple.setOnAction(event -> );
 
         info = new MenuItem("Autores del programa");
-        info.setOnAction(event -> new Info());
+        create_info();
 
         abiertos = new Menu("Abiertos");
         abiertos.getItems().addAll(newtonRhapson);
@@ -59,11 +65,32 @@ public class HelloApplication extends Application
         ecuaciones.getItems().addAll(abiertos, cerrados, lineales);
 
         menu = new MenuBar();
-        menu.getMenus().addAll(ecuaciones, regresion, extra);
+        menu.getMenus().addAll(ecuaciones, regresion, extra, botonSalir);
         vbox = new VBox(menu);
         escena = new Scene(vbox);
         escena.getStylesheets().add(getClass().getResource("/css/general.css").toExternalForm());
     }
+
+    public void create_info()
+    {
+        infoContextMenu = new ContextMenu();
+        Label contenido = new Label("Programa creado por alumnos del Tecnológico Nacional de México en Celaya para la" +
+                " materia de métodos numéricos\nIntegrantes:\n-Arriaga Vázquez Mariana\n-González Cervantes Esteban\n" +
+                "-Hernández Luna Diego Leonardo\n-Rivera Torres Juan Angel\n-Silva Paniagua Jennifer María");
+        contenido.setWrapText(true);
+        CustomMenuItem customMenuItem = new CustomMenuItem(contenido, false);
+        customMenuItem.setId("menu-item-info");
+        infoContextMenu.getItems().add(customMenuItem);
+
+        info.setOnAction(e -> {
+            if (!infoContextMenu.isShowing()) {
+                infoContextMenu.show(menu, Side.BOTTOM, 0, 0);
+            } else {
+                infoContextMenu.hide();
+            }
+        });
+    }
+
     @Override
     public void start(Stage stage) throws IOException
     {
