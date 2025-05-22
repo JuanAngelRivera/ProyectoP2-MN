aa8acf4cc54da15e0025fde138f99767548bcce6
Cherrisita <juangelrt21@gmail.com>
2025-05-22 14:46:57 -0600

    Lineal y Lineal multiple formato

--- src/main/java/com/example/proyecto2pmn/RegresionMultiple/LinealMultiple.java
+++ src/main/java/com/example/proyecto2pmn/RegresionMultiple/LinealMultiple.java
@@ -4,15 +4,23 @@ import com.example.proyecto2pmn.Ecuacion;
 
 import static java.lang.Math.sqrt;
 
-public class LinealMultiple extends Ecuacion {
+public class LinealMultiple extends Ecuacion
+{
     int n;
 
-    public LinealMultiple() {
+    public LinealMultiple()
+    {
         super("Regresion lineal Multiple");
-        super.descripcion = "Regresion lineal Multiple";
+        super.descripcion = "La regresión lineal múltiple es una extensión de la regresión lineal simple, en la que se modela"
+    + " la relación entre una variable dependiente y dos o más variables independientes. El objetivo sigue siendo"
+    + " ajustar un modelo que minimice los errores, pero en este caso se usa un hiperplano en lugar de una recta.\nEste método"
+    + " también se desarrolló a partir de los trabajos de Galton y Pearson, pero se generalizó en el siglo XX con el "
+    + "desarrollo de la estadística moderna y el álgebra matricial. Hoy en día, es una técnica central en análisis de datos,"
+    + " economía, psicología, ingeniería y muchas otras disciplinas.";
     }
 
-    public void Metodomultiple(int n,String y, String x1, String x2) {
+    public void Metodomultiple(int n,String y, String x1, String x2)
+    {
         //crear arreglo n renglones por 10 columnas
         //y x1 x2 x1*x1 x1*x2 .....
         this.n = n;
@@ -22,12 +30,16 @@ public class LinealMultiple extends Ecuacion {
         datos = entrada.split(" ");
         int count = 0;
         double[][] tabla = new double[n + 1][10];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < n; j++) {
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
                 tabla[j][i] = Double.parseDouble(datos[count]);
                 count++;
             }
         }
+
         //ir llenando las columnas de la tabla
         tabla = elevarX1(tabla);
         tabla = multiplicarX1x2(tabla);
@@ -43,34 +55,36 @@ public class LinealMultiple extends Ecuacion {
         resultado[1] = tabla[n][6];
         resultado[2] = tabla[n][7];
 
-        ecuaciones[0][0]=n;
-        ecuaciones[0][1]=tabla[n][1];
-        ecuaciones[0][2]=tabla[n][2];
-        ecuaciones[1][0]=tabla[n][1];
-        ecuaciones[1][1]=tabla[n][3];
-        ecuaciones[1][2]=tabla[n][4];
-        ecuaciones[2][0]=tabla[n][2];
-        ecuaciones[2][1]=tabla[n][4];
-        ecuaciones[2][2]=tabla[n][5];
+        ecuaciones[0][0] = n;
+        ecuaciones[0][1] = tabla[n][1];
+        ecuaciones[0][2] = tabla[n][2];
+        ecuaciones[1][0] = tabla[n][1];
+        ecuaciones[1][1] = tabla[n][3];
+        ecuaciones[1][2] = tabla[n][4];
+        ecuaciones[2][0] = tabla[n][2];
+        ecuaciones[2][1] = tabla[n][4];
+        ecuaciones[2][2] = tabla[n][5];
+
         mostrarEcuaciones(resultado,ecuaciones);
         double[] solucionEc;
-        solucionEc=resolverSistema(ecuaciones,resultado);
+        solucionEc = resolverSistema(ecuaciones,resultado);
 
-        tabla=calcularSr(tabla,solucionEc);
-        double promedioy=tabla[n][0]/n;
-        tabla=calcularSt(tabla,promedioy);
+        tabla = calcularSr(tabla, solucionEc);
+        double promedioy = tabla[n][0] / n;
+        tabla = calcularSt(tabla, promedioy);
         mostrar(tabla);
         mostrarSoluciones(solucionEc);
-        double coefCorr=sqrt((tabla[n][9]-tabla[n][8])/tabla[n][9]);
+        double coefCorr = sqrt((tabla[n][9]-tabla[n][8]) / tabla[n][9]);
         System.out.print("COEFICIENTE DE CORRELACION: ");
         System.out.printf("%.6f",coefCorr);
-
-
     }
 
-    public void mostrar(double  [][] tabla){
-        for (int i = 0; i < tabla.length; i++) {
-            for (int j = 0; j < tabla[i].length; j++) {
+    public void mostrar(double  [][] tabla)
+    {
+        for (int i = 0; i < tabla.length; i++)
+        {
+            for (int j = 0; j < tabla[i].length; j++)
+            {
                 System.out.printf("%.6f",tabla[i][j]);
                 System.out.print(" ");
             }
@@ -78,62 +92,85 @@ public class LinealMultiple extends Ecuacion {
         }
 
     }
-    public void mostrarEcuaciones(double[] resultados, double[][] coeficientes) {
+
+    public void mostrarEcuaciones(double[] resultados, double[][] coeficientes)
+    {
         System.out.println("Ecuaciones");
-        for (int i = 0; i < resultados.length; i++) {
-            for (int j = 0; j < coeficientes[i].length; j++) {
+        for (int i = 0; i < resultados.length; i++)
+        {
+            for (int j = 0; j < coeficientes[i].length; j++)
+            {
                 //System.out.print(coeficientes[i][j]+"a"+j+" + ");
-                if(j+1<coeficientes[i].length){
-                    System.out.print(coeficientes[i][j]+"a"+j+" + ");
+                if(j+1<coeficientes[i].length)
+                {
+                    System.out.print(coeficientes[i][j] + "a" + j + " + ");
                 }
                 else
-                    System.out.print(coeficientes[i][j]+"a"+j);
+                    System.out.print(coeficientes[i][j] + "a" + j);
             }
-            System.out.print(" = "+resultados[i]);
+            System.out.print(" = " + resultados[i]);
             System.out.println();
         }
     }
-    public void mostrarSoluciones(double[] solucion) {
+
+    public void mostrarSoluciones(double[] solucion)
+    {
         System.out.println("Soluciones de ecuaciones");
-        for (double v : solucion) {
+        for (double v : solucion)
+        {
             System.out.println(v);
         }
     }
-    double [][] elevarX1(double [][] tabla){
-        for (int i =0 ; i < n; i++) {
+
+    double [][] elevarX1(double [][] tabla)
+    {
+        for (int i =0 ; i < n; i++)
+        {
             tabla[i][3]=tabla[i][1]*tabla[i][1];
         }
         return tabla;
     }
 
-    double [][] multiplicarX1x2(double [][] tabla){
-        for (int i =0 ; i < n; i++) {
+    double [][] multiplicarX1x2(double [][] tabla)
+    {
+        for (int i =0 ; i < n; i++)
+        {
             tabla[i][4]=tabla[i][1]*tabla[i][2];
         }
         return tabla;
     }
-    double [][] elevarX2(double [][] tabla){
-        for (int i =0 ; i < n; i++) {
+
+    double [][] elevarX2(double [][] tabla)
+    {
+        for (int i =0 ; i < n; i++)
+        {
             tabla[i][5]=tabla[i][2]*tabla[i][2];
         }
         return tabla;
     }
-    double [][] multiplicarX1Y(double [][] tabla){
-        for (int i =0 ; i < n; i++) {
+    double [][] multiplicarX1Y(double [][] tabla)
+    {
+        for (int i =0 ; i < n; i++)
+        {
             tabla[i][6]=tabla[i][0]*tabla[i][1];
         }
         return tabla;
     }
-    double [][] multuplicarX2Y(double [][] tabla){
-        for (int i =0 ; i < n; i++) {
+    double [][] multuplicarX2Y(double [][] tabla)
+    {
+        for (int i =0 ; i < n; i++)
+        {
             tabla[i][7]=tabla[i][0]*tabla[i][2];
         }
         return tabla;
     }
-    double [][] suma(double [][] tabla){
+    double [][] suma(double [][] tabla)
+    {
         double suma=0;
-        for (int i =0 ; i < 10; i++) {
-            for (int j =0 ; j < n; j++) {
+        for (int i =0 ; i < 10; i++)
+        {
+            for (int j =0 ; j < n; j++)
+            {
                 suma+=tabla[j][i];
             }
             tabla[n][i]=suma;
@@ -141,31 +178,39 @@ public class LinealMultiple extends Ecuacion {
         }
         return tabla;
     }
-    double [][] calcularSr(double [][] tabla,double []soluciones){
-        for (int i =0 ; i < n; i++) {
+
+    double [][] calcularSr(double [][] tabla,double []soluciones)
+    {
+        for (int i =0 ; i < n; i++)
+        {
             tabla[i][8]=Math.pow(tabla[i][0]-soluciones[0]-soluciones[1]*tabla[i][1]-soluciones[2]*tabla[i][2],2);
         }
         suma(tabla);
         return tabla;
-
     }
-    double [][] calcularSt(double [][] tabla,double promedio){
-        for (int i =0 ; i < n; i++) {
+    double [][] calcularSt(double [][] tabla,double promedio)
+    {
+        for (int i =0 ; i < n; i++)
+        {
             tabla[i][9]=Math.pow(tabla[i][0]-promedio,2);
             suma(tabla);
         }
         return tabla;
     }
 
-    public static double[] resolverSistema(double[][] matriz, double[] resultados) {
+    public static double[] resolverSistema(double[][] matriz, double[] resultados)
+    {
         int n = 3;
-
         // Formamos la matriz aumentada
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             // Hacemos que el pivote sea distinto de cero
-            if (matriz[i][i] == 0) {
-                for (int j = i + 1; j < n; j++) {
-                    if (matriz[j][i] != 0) {
+            if (matriz[i][i] == 0)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (matriz[j][i] != 0)
+                    {
                         double[] temp = matriz[i];
                         matriz[i] = matriz[j];
                         matriz[j] = temp;
@@ -179,9 +224,11 @@ public class LinealMultiple extends Ecuacion {
             }
 
             // Eliminación hacia adelante
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < n; j++)
+            {
                 double factor = matriz[j][i] / matriz[i][i];
-                for (int k = i; k < n; k++) {
+                for (int k = i; k < n; k++)
+                {
                     matriz[j][k] -= factor * matriz[i][k];
                 }
                 resultados[j] -= factor * resultados[i];
@@ -190,9 +237,11 @@ public class LinealMultiple extends Ecuacion {
 
         // Sustitución hacia atrás
         double[] soluciones = new double[n];
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--)
+        {
             double suma = resultados[i];
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < n; j++)
+            {
                 suma -= matriz[i][j] * soluciones[j];
             }
             soluciones[i] = suma / matriz[i][i];
@@ -203,22 +252,20 @@ public class LinealMultiple extends Ecuacion {
 
 
     @Override
-    public double obtenerRaiz() {
+    public double obtenerRaiz()
+    {
         return 0;
     }
 
     @Override
-    public double obtenerErrorU() {
+    public double obtenerErrorU()
+    {
         return 0;
     }
 
     @Override
-    public void valoresParametro(Double[] parametros) {
-
-    }
+    public void valoresParametro(Double[] parametros){}
 
     @Override
-    public void calcularIteraciones() {
-
-    }
+    public void calcularIteraciones() {}
 }
